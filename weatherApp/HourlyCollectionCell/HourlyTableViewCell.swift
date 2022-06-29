ae877c6214a5ca41ef0e5fa7fe142d53139bde2d
truemilkoooo <truemilkoooo@gmail.com>
2022-06-29 09:23:22 +0300

    try

---
+++ weatherApp/HourlyCollectionCell/HourlyTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  HourlyTableViewCell.swift
+//  weatherApp
+//
+//  Created by Вова Пупкин on 25.06.2022.
+//
+
+import UIKit
+
+class HourlyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    static let identifier = "HourlyTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "HourlyTableViewCell", bundle: nil)
+    }
+    
+}
--- weatherApp/ViewController.swift
+++ weatherApp/ViewController.swift
@@ -6,14 +6,196 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
+    
+    @IBOutlet var table: UITableView!
+    var models = [DailyWeatherEntry]()
+    
+    let locationManager = CLLocationManager()
+    
+    var currentLocation: CLLocation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        //register 2 cells
+        table.register(HourlyTableViewCell.nib(), forCellReuseIdentifier: HourlyTableViewCell.identifier)
+        table.register(WeatherTableViewCell.nib(), forCellReuseIdentifier: WeatherTableViewCell.identifier)
 
+        
+        table.delegate = self
+        table.dataSource = self
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupLocation()
+        
+    }
+    //location
+    func setupLocation() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if !locations.isEmpty, currentLocation == nil {
+            currentLocation = locations.first
+            locationManager.stopUpdatingLocation()
+            requestWeatherForLocation()
+        }
+    }
+    
+    func requestWeatherForLocation() {
+        guard let currentLocation = currentLocation else {
+            return
+        }
+        let long = currentLocation.coordinate.longitude
+        let lat = currentLocation.coordinate.latitude
+        
+        let url = "https://api.gismeteo.net/v2/weather/current/\(lat)&\(long)"
+
+        URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
+            //validation
+            guard let data = data, error == nil else {
+                print ("Something went wrong")
+                return
+            }
+            
+            //convert data to model
+            
+            var json: WeatherResponse?
+            do {
+                json = try JSONDecoder().decode(WeatherResponse.self, from: data)
+            }
+            catch {
+                print ("error: \(error)")
+            }
+            
+            guard let result = json else {
+                return
+            }
+            print (result.currently.summary)
+            //update user interface
+        }).resume()
+    }
+    
+    //table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 
 }
 
+struct WeatherResponse: Codable {
+    let latitude: Float
+    let longitude: Float
+    let timezone: String
+    let currently: CurrentWeather
+    let hourly: HourlyWeather
+    let daily: DailyWeather
+    let offset: Float
+}
+
+struct CurrentWeather: Codable {
+    let time: Int
+    let summary: String
+    let icon: String
+    let nearestStormDistanse: Int
+    let nearestStormBearing: Int
+    let precipIntensity: Int
+    let precipProbability: Int
+    let temperature: Double
+    let apparentTemperature: Double
+    let dewPoint: Double
+    let humidity: Double
+    let pressure: Double
+    let windSpeed: Double
+    let windGust: Double
+    let windBearing:Int
+    let cloudCover: Double
+    let uvIndex: Int
+    let visibility: Double
+    let ozone: Double
+}
+
+struct DailyWeather: Codable {
+    let summary: String
+    let icon: String
+    let data: [DailyWeatherEntry]
+}
+
+struct DailyWeatherEntry: Codable {
+    let time: Int
+    let summary: String
+    let icon: String
+    let sunriseTime: Int
+    let sunset: Int
+    let moonPhase: Double
+    let precipIntensity: Float
+    let precipIntensityMax: Float
+    let precipIntensityMaxTime: Int
+    let precipProbability: Double
+    let precipType: String?
+    let temperatureHigh: Double
+    let temperatureHighTime: Int
+    let temperatureLow: Double
+    let temperatureLowTime: Int
+    let apparentTemperatureHigh: Double
+    let apparentTemperatureHighTime: Int
+    let apparentTemperatureLow: Double
+    let apparentTemperaturelowTime: Int
+    let dewPoint: Double
+    let humidity: Double
+    let pressure: Double
+    let windSpeed: Double
+    let windGust: Double
+    let windGustTime: Int
+    let windBearing: Int
+    let cloudCover: Double
+    let uvIndex: Int
+    let uvUndexTime: Int
+    let visibility: Double
+    let ozone: Double
+    let temperatureMin: Double
+    let temperatureMinTime: Int
+    let temperaturemax: Double
+    let temperatureMaxTime: Int
+    let apparentTemperatureMin: Double
+    let apparentTemperatureMinTime: Int
+    let apparentTemperatureMax: Double
+    let apparentTemperatureMaxTime: Int
+}
+struct HourlyWeather: Codable {
+    let summary: String
+    let icon: String
+    let data: [HourlyWeatherEntry]
+}
+struct HourlyWeatherEntry: Codable {
+    let time: Int
+    let summary: String
+    let icon: String
+    let precipIntensity: Float
+    let precipProbability: Double
+    let precipType: String?
+    let temperature: Double
+    let apparentTemperature: Double
+    let dewPoint: Double
+    let humidity: Double
+    let pressure: Double
+    let windSpeed: Double
+    let windGust: Double
+    let windBearing: Int
+    let cloudCover: Double
+    let uvIndex: Int
+    let visibility: Double
+    let ozone: Double
+}
